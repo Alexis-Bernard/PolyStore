43de491fa08781cd9e8e91b95a6080a30a8cc131
Alexis-Bernard <alexisbernard34310@gmail.com>
2023-05-30 15:58:50 +0200

    remove: default tests

--- inventory/src/test/java/fr/polytech/polystore/inventory/InventoryApplicationTests.java
+++ inventory/src/test/java/fr/polytech/polystore/inventory/InventoryApplicationTests.java
@@ -1,13 +1,8 @@
 package fr.polytech.polystore.inventory;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InventoryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
